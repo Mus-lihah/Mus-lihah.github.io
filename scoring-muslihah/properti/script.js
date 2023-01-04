dee926aced6888508c35179096446e77e3acced6
Muslihah <113479745+Mus-lihah@users.noreply.github.com>
2023-01-04 10:10:30 +0700

    Update script.js

--- scoring-muslihah/properti/script.js
+++ scoring-muslihah/properti/script.js
@@ -1,32 +1,40 @@
-const sumRight = document.querySelector(".sum__right");
-const sumLeft = document.querySelector(".sum__left");
+const score1 = document.getElementById("score1");
+const score2 = document.getElementById("score2");
 
-sumRight.addEventListener("click", handleClick);
-sumRight.addEventListener("touchstart", handleTouchStart);
-sumRight.addEventListener("touchend", handleTouchEnd);
+let prevY; // variable to store the previous y-coordinate of the touch point
 
-sumLeft.addEventListener("click", handleClick);
-sumLeft.addEventListener("touchstart", handleTouchStart);
-sumLeft.addEventListener("touchend", handleTouchEnd);
-
-function handleClick(event) {
-  if (event.offsetY < this.offsetHeight / 2) {
-    this.textContent = parseInt(this.textContent) + 1;
-  } else if (event.offsetY >= this.offsetHeight / 2 && parseInt(this.textContent) > 0) {
-    this.textContent = parseInt(this.textContent) - 1;
-  }
-}
+// add touch start event listener to the scores
+score1.addEventListener("touchstart", handleTouchStart);
+score2.addEventListener("touchstart", handleTouchStart);
 
 function handleTouchStart(event) {
-  this.touchStartY = event.touches[0].clientY;
+  // get the y-coordinate of the touch point
+  prevY = event.touches[0].clientY;
 }
 
-function handleTouchEnd(event) {
-  const touchDeltaY = this.touchStartY - event.changedTouches[0].clientY;
+// add touch move event listener to the scores
+score1.addEventListener("touchmove", handleTouchMove);
+score2.addEventListener("touchmove", handleTouchMove);
 
-  if (touchDeltaY > 0 && this.textContent > 0) {
-    this.textContent = parseInt(this.textContent) - 1;
-  } else if (touchDeltaY < 0) {
+function handleTouchMove(event) {
+  // prevent default scrolling behavior
+  event.preventDefault();
+
+  // get the current y-coordinate of the touch point
+  const currentY = event.touches[0].clientY;
+
+  // calculate the difference between the current and previous y-coordinates
+  const diffY = prevY - currentY;
+
+  // update the score based on the direction of the swipe
+  if (diffY > 0) {
+    // swipe up
     this.textContent = parseInt(this.textContent) + 1;
+  } else if (diffY < 0 && this.textContent > 0) {
+    // swipe down
+    this.textContent = parseInt(this.textContent) - 1;
   }
+
+  // update the previous y-coordinate
+  prevY = currentY;
 }
