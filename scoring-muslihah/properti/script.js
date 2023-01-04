c0cb0e7410bf5e3045bc8335abb1bb082978bae0
Muslihah <113479745+Mus-lihah@users.noreply.github.com>
2023-01-04 10:17:16 +0700

    Update script.js

--- scoring-muslihah/properti/script.js
+++ scoring-muslihah/properti/script.js
@@ -2,6 +2,7 @@ const score1 = document.getElementById("score1");
 const score2 = document.getElementById("score2");
 
 let prevY; // variable to store the previous y-coordinate of the touch point
+let targetScore; // variable to store the target score
 
 // add touch start event listener to the scores
 score1.addEventListener("touchstart", handleTouchStart);
@@ -10,6 +11,8 @@ score2.addEventListener("touchstart", handleTouchStart);
 function handleTouchStart(event) {
   // get the y-coordinate of the touch point
   prevY = event.touches[0].clientY;
+  // store the initial score as the target score
+  targetScore = parseInt(this.textContent);
 }
 
 // add touch move event listener to the scores
@@ -26,13 +29,13 @@ function handleTouchMove(event) {
   // calculate the difference between the current and previous y-coordinates
   const diffY = prevY - currentY;
 
-  // update the score based on the direction of the swipe
+  // update the target score based on the direction of the swipe
   if (diffY > 0) {
     // swipe up
-    this.textContent = parseInt(this.textContent) + 1;
-  } else if (diffY < 0 && this.textContent > 0) {
+    targetScore++;
+  } else if (diffY < 0 && targetScore > 0) {
     // swipe down
-    this.textContent = parseInt(this.textContent) - 1;
+    targetScore--;
   }
 
   // update the previous y-coordinate
@@ -46,5 +49,25 @@ score2.addEventListener("touchend", handleTouchEnd);
 function handleTouchEnd(event) {
   // reset the previous y-coordinate
   prevY = null;
+
+  // start the animation loop
+  animateScore();
 }
 
+function animateScore() {
+  // check if the current score is the same as the target score
+  if (parseInt(this.textContent) === targetScore) {
+    // stop the animation loop
+    return;
+  }
+
+  // update the current score
+  if (parseInt(this.textContent) < targetScore) {
+    this.textContent = parseInt(this.textContent) + 1;
+  } else {
+    this.textContent = parseInt(this.textContent) - 1;
+  }
+
+  // schedule the next animation frame
+  requestAnimationFrame(animateScore.bind(this));
+}
